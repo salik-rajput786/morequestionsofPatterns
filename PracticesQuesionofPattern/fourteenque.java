5fee11129ec824ec4c9571fd374066b71fdbc28f
salik-rajput786 <126151200+salik-rajput786@users.noreply.github.com>
2024-01-21 12:24:52 +0530

    Update fourteenque.java

--- PracticesQuesionofPattern/fourteenque.java
+++ PracticesQuesionofPattern/fourteenque.java
@@ -5,6 +5,8 @@ package PracticesQuesionofPattern;
  1 2       2 1
  1           1
  */
+// THIS CODE WILL WORK FOR 1 TO 10 NUMBER ONLT 
+// SO GET N= 1 TO 10 ONLY 
 import java.util.Scanner;
 
 public class fourteenque {
